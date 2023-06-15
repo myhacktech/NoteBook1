36b06cab6025be366a9521dd8a809bbe4798beb6
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2023-06-15 22:57:29 +0530

    ok backend

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -1,21 +1,27 @@
-var jwt = require('jsonwebtoken');
-const secret = "H@ppiness starts with y0u";
-
+var jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+const secret = process.env.SECRET_KEY;
 
 const fetchuser = (req, res, next) => {
-    // get the user from jwt token and append id to req object
-    const token = req.header("auth-token");
-    if (!token) {
-        return res.status(401).send({ error: "Please use a valid token" })
-    } 
-    try{ 
+  // get the user from jwt token and append user_id to req object
+  const token = req.header("auth-token");
+
+  if (!token) {
+    return res.status(401).send({ error: "Please use a valid token" });
+  }
+  try {
+    // here data is the payload(user_id) from which token is formed
+    // here we take token from request header and then we verify with secret_key 
+    // and take out the payload(user_id) from token using secret_key
+    // add user_id in request body
+
     const data = jwt.verify(token, secret);
     req.user = data.user;
-    
-    next();
-    } catch (error) {
-        res.status(401).send({ error: "Please use a valid token" })
-    }
-}
+    next(); // then process the next step
+  } catch (error) {
+    res.status(401).send({ error: "Please use a valid token" });
+  }
+};
 
-module.exports = fetchuser
+module.exports = fetchuser;
