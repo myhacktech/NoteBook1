b7adbdbd357c78712521122db2b7ba56b101371e
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2023-06-15 22:45:47 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light"  style={{backgroundColor: "#e3f2fd",opacity:"70%"}}>
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">iNOTEBOOK</a>
+                    <a className="navbar-brand" href="#">NOTEBOOK</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -37,7 +37,9 @@ function Navbar() {
 
                         {!localStorage.getItem('token') ?
                             <form className='d-flex'>
+                            
                                 <Link to="/login" className={`nav-link ${location.pathname === "/login" ? "active" : ""}`} >Login</Link>
+
                                 <Link to="/signup" className={`nav-link ${location.pathname === "/signup" ? "active" : ""}`} >SignUp</Link>
                             </form> :
                             <Link to="/login" onClick={handleLogout} className={`nav-link ${location.pathname === "/logout" ? "active" : ""}`} >Logout</Link>
