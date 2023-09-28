5054c2c0875769e9872a681089ab17e66fb52125
Rajat Singh <85948834+Rajat2024@users.noreply.github.com>
2023-09-28 11:09:15 +0530

    ok

--- database .js
+++ database .js
@@ -4,14 +4,18 @@ dotenv.config();
 const MONGO_URL = process.env.MONGO_URL;
 const connectToMongo = async () => {
   try {
-    const conn = await mongoose.connect(MONGO_URL); // connecting to database
+    mongoose.set("strictQuery", false);
+
+    const conn = await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }); // connecting to database
     console.log(`MongoDB Connected: ${conn.connection.host} `); // if connected
   } catch (error) {
     console.error(`Error: ${error}`);
     // if not connected then exit the process
-    process.exit(); 
+    process.exit();
   }
 };
-
 
 module.exports = connectToMongo; // exporting the function
